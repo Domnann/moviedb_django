61ff7c27aeb460d7bbe3f5ae4b4dd256863e7211
Domnann <domnangambo@gmail.com>
2022-07-06 23:34:40 +0100

    worked on views and template

--- movieapp/movie/urls.py
+++ movieapp/movie/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='moviesite'),
-    path('result/', views.result, name='moviesite-result'),
+    path('', views.index, name='moviesite'),
 ]
--- movieapp/movie/views.py
+++ movieapp/movie/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pprint
+import requests
+import urllib.request
+import pandas as pd
+
+# posts = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# def home(request):
+    
+#     return render(request, 'movie/home.html')
 
 
-def home(request):
-    return render(request, 'movie/home.html')
+
+# def result(request):
+#     context ={
+#         "post": posts
+#     }
+#     return render(request, 'movie/result.html', {'title': 'Result'})
+
+api_version = 3
+api_key = "99558ae618ca3adc4cfcf6b988a999dd"
+api_base_url = f"https://api.themoviedb.org/{api_version}"
+endpoint_path = f"/search/multi"
 
 
-
-def result(request):
-    return HttpResponse('<h1>hello world 2</h1>')
+def index(request):
+    if request.method == 'POST':
+        searh_query = request.POST['search']
+        endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}&query={searh_query}&language=en-US"
+        # print(endpoint)
+        r = requests.get(endpoint)
+        # pprint.pprint(r.json())
+        if r.status_code in range(200, 299):
+            data = r.json()
+            results = data['results']
+            if len(results) > 0:
+                print(results[0].keys())
+                for result in results:
+                    _id = result['id']
+                    print(result['title'],"ID:", _id,"\nRelease Date:",result['release_date'],"\nOverview:" ,result['overview'],"\nVote Average:", result['vote_average'] ,"\n\n")
+    return render(request, "movie/index.html")
